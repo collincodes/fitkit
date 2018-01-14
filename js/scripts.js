599ba1cb574550417a7af0f62210be48f211eb56
cmdeveloped <cmwebdevs@gmail.com>
2018-01-14 01:35:52 -0600

    Initial DEE calculations finished.

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,43 @@ $(window).scroll(function() {
   }
 });
 
+// Tabs to choose calculator
+$('#fitTabs .nav-item:first-child .nav-link').tab('show');
+
+// Daily Energy Expenditure Calculator
+$('.gender .pick:first-child').addClass('active');
+$('.gender .pick').click(function() {
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+});
+
+$('.submit-expend').click(function() {
+  $('.expend-calc').empty();
+  var $expendFeet = $('.feet input[name="feet"]').val();
+  var $expendInch = $('.inches input[name="inches"]').val();
+  var $ftConv = parseInt($expendFeet) * 30.48;
+  var $inConv = parseInt($expendInch) * 2.54;
+  var $height = $ftConv + $inConv;
+
+  var $expendWeight = $('.weight input[name="weight"]').val();
+  var $weightConv = parseInt($expendWeight) / 2.2;
+  var $kg = $weightConv
+
+  var $expendAge = $('.age input[name="age"]').val();
+  var $age = parseInt($expendAge);
+
+  var $expendMale = ((10 * $kg) + (6.25 * $height) - (5 * $age) + 5);
+  var $expendFemale = ((10 * $kg) + (6.25 * $height) - (5 * $age) - 161);
+
+  if ( $('.male').hasClass('active') && $('.weight input').val() && $('.age input').val() && $('.feet input').val() && $('.inches input').val()) {
+    $('.expend-calc').append($expendMale.toFixed(1) + ' Calories');
+  } else if ( $('.female').hasClass('active') && $('.weight input').val() && $('.age input').val() && $('.feet input').val() && $('.inches input').val()) {
+    $('.expend-calc').append($expendFemale.toFixed(1) + ' Calories');
+  } else {
+    $('.expend-calc').append('please fill out all fields.');
+  }
+
+  $('.expend-calc').addClass('animated-expend');
+});
+
 });
