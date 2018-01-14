2b682a5a26c4c0f0bd7a3e50d65f6d3b352c1148
cmdeveloped <cmwebdevs@gmail.com>
2018-01-14 15:36:44 -0600

    Completed daily energy expenditure calculator - for now

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,45 @@ $('.gender .pick').click(function() {
   $(this).siblings().removeClass('active');
 });
 
+$('.activity .pick:first-child').addClass('active');
+$('.activity .pick').click(function() {
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+
+  var $clear = $('.input-message').empty();
+
+  if ($('.sedentary').hasClass('active')) {
+    $clear;
+    $('.input-message').append('LITTLE TO NO DAILY ACTIVITY');
+  } else if ($('.lightly').hasClass('active')) {
+    $clear;
+    $('.input-message').append('ACTIVE 1-3 DAYS PER WEEK');
+  } else if ($('.moderately').hasClass('active')) {
+    $clear;
+    $('.input-message').append('ACTIVE 3-5 DAYS PER WEEK');
+  } else if ($('.heavily').hasClass('active')) {
+    $clear;
+    $('.input-message').append('ACTIVE 6-7 DAYS PER WEEK');
+  }
+});
+
+// Update Input Message
+$('.variable input[name="weight"]').focus(function() {
+  $('.input-message').empty();
+  $('.input-message').append('Enter Weight in Pounds');
+});
+
+$('.variable input[name="age"]').focus(function() {
+  $('.input-message').empty();
+  $('.input-message').append('Enter Age in Years');
+});
+
+$('.variable input[name="feet"]').focus(function() {
+  $('.input-message').empty();
+  $('.input-message').append('Enter Height in Feet & Inches');
+});
+
+
 $('.submit-expend').click(function() {
   $('.expend-calc').empty();
   var $expendFeet = $('.feet input[name="feet"]').val();
@@ -37,9 +76,33 @@ $('.submit-expend').click(function() {
   var $expendFemale = ((10 * $kg) + (6.25 * $height) - (5 * $age) - 161);
 
   if ( $('.male').hasClass('active') && $('.weight input').val() && $('.age input').val() && $('.feet input').val() && $('.inches input').val()) {
-    $('.expend-calc').append($expendMale.toFixed(1) + ' Calories');
+    if ( $('.sedentary').hasClass('active') ) {
+      var $sedentaryMale = ($expendMale * 1.2);
+      $('.expend-calc').append($sedentaryMale.toFixed(1) + ' Calories');
+    } else if ( $('.lightly').hasClass('active') ) {
+      var $lightlyMale = ($expendMale * 1.375);
+      $('.expend-calc').append($lightlyMale.toFixed(1) + ' Calories');
+    } else if ( $('.moderately').hasClass('active') ) {
+      var $moderatelyMale = ($expendMale * 1.55);
+      $('.expend-calc').append($moderatelyMale.toFixed(1) + ' Calories');
+    } else if ( $('.heavily').hasClass('active') ) {
+      var $heavilyMale = ($expendMale * 1.725);
+      $('.expend-calc').append($heavilyMale.toFixed(1) + ' Calories');
+    }
   } else if ( $('.female').hasClass('active') && $('.weight input').val() && $('.age input').val() && $('.feet input').val() && $('.inches input').val()) {
-    $('.expend-calc').append($expendFemale.toFixed(1) + ' Calories');
+    if ( $('.sedentary').hasClass('active') ) {
+      var $sedentaryFemale = ($expendFemale * 1.2);
+      $('.expend-calc').append($sedentaryFemale.toFixed(1) + ' Calories');
+    } else if ( $('.lightly').hasClass('active') ) {
+      var $lightlyFemale = ($expendFemale * 1.375);
+      $('.expend-calc').append($lightlyFemale.toFixed(1) + ' Calories');
+    } else if ( $('.moderately').hasClass('active') ) {
+      var $moderatelyFemale = ($expendFemale * 1.55);
+      $('.expend-calc').append($moderatelyFemale.toFixed(1) + ' Calories');
+    } else if ( $('.heavily').hasClass('active') ) {
+      var $heavilyFemale = ($expendFemale * 1.725);
+      $('.expend-calc').append($heavilyFemale.toFixed(1) + ' Calories');
+    }
   } else {
     $('.expend-calc').append('please fill out all fields.');
   }
